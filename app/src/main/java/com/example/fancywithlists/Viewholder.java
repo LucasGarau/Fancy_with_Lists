624cac80aa74325d0b476c615fa4fe6863696e6c
LucasGarau <Lucas1012000>
2020-01-30 19:27:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fancywithlists/Viewholder.java
@@ -0,0 +1,11 @@
+package com.example.fancywithlists;
+import android.view.View;
+import android.widget.ImageView;
+
+class ViewHolder {
+    ImageView icon=null;
+
+    ViewHolder(View base) {
+        this.icon=(ImageView)base.findViewById(R.id.icon);
+    }
+}
